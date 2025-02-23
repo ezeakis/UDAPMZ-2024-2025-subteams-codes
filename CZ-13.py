f8fbe5208a4f30b3327a5a2334e74157d8577e3d
ezeakis <ezeakis@gmail.com>
2025-02-24 00:38:13 +0200

    Update CZ-13.py

--- CZ-13.py
+++ CZ-13.py
@@ -11,28 +11,44 @@ sense.color.gain = 60 # Nastav citlivost senzoru
 sense.color.integration_cycles = 64 # Interval, který udává frekvenci měření ze senzoru
 
 # Přidej proměnné s barvami a obrázek
-c=(0, 0, 0)
+c=(255, 255, 255)
 G=(255, 120, 53)
 Y=(142, 255, 103)
 AD=(0, 128, 0)
 AH=(0, 0, 0)
 AR=(155, 255, 80)
+D=(0, 0, 255)
+L=(0, 0, 0)
+AB=(0, 128, 0)
 
-for i in range(28):
+
+for i in range(7):
     rgb=sense.color
     c=(rgb.red,rgb.green,rgb.blue)
     
-    obrazek = [
-      c, c, c, G, G, c, c, c,
-      c, c, G, G, G, G, c, c,
-      c, G, G, G, G, Y, Y, c,
-      c, c, AD, AD, G, Y, AH, Y,
-      c, AD, AD, AD, AD, Y, Y, Y,
-      AR, AD, AD, AD, AD, Y, Y, c,
-      c, AR, AR, AR, AR, c, c, c,
-      c, AR, c, c, AR, c, c, c]
+    obrazek1 = [
+        c, c, c, G, G, c, c, c,
+        c, c, G, G, G, G, c, c,
+        c, G, G, G, G, Y, Y, c,
+        c, c, AD, AD, G, Y, AH, Y,
+        c, AD, AD, AD, AD, Y, Y, Y,
+        AR, AD, AD, AD, AD, Y, Y, c,
+        c, AR, AR, AR, AR, c, c, c,
+        c, AR, c, c, AR, c, c, c]
     
-    sense.set_pixels(obrazek)
-    sleep(1)
-# Zobraz obrázek
+    obrazek2 = [
+        D, D, D, D, c, c, D, D,
+        L, D, L, c, c, c, c, D,
+        L, L, L, D, c, c, D, L,
+        AB, L, AB, D, D, D, D, L,
+        L, L, L, L, L, L, L, L,
+        D, L, L, L, L, L, L, L,
+        D, L, D, L, D, L, D, L,
+        AB, L, AB, L, AB, L, AB, L]
+    
+    sense.set_pixels(obrazek1)
+    sleep(2)
+
+    sense.set_pixels(obrazek2)
+    sleep(2)
 
